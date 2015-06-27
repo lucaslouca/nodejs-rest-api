64b5cab618fada3f9335216e1a28176e1fda331b
Lucas Louca <lucas@lucaslouca.com>
2015-06-27 09:39:51 +0200

    stripped whitespaces

--- rest-api/routes/api.js
+++ rest-api/routes/api.js
@@ -11,9 +11,9 @@ var theport = process.env.PORT || 27017;
 // operations and release them when the connection is complete.
 mongoose.connect(uristring, function (err, res) {
   if (err) {
-	console.log ('ERROR connecting to: ' + uristring + '. ' + err);
+    console.log ('ERROR connecting to: ' + uristring + '. ' + err);
   } else {
-	console.log ('Succeeded connected to: ' + uristring);
+    console.log ('Succeeded connected to: ' + uristring);
   }
 });
 
@@ -25,15 +25,15 @@ var messageSchema = new mongoose.Schema({
 var Message = mongoose.model('FluxStepMessage', messageSchema);
 
 // Retrieve all messages from the database
-// Test it using curl: curl -i -X GET http://localhost:3000/messages	
+// Test it using curl: curl -i -X GET http://localhost:3000/messages
 exports.findAll = function(req, res) {
     Message.find({}).exec(function(err, result) {
-	  if (err) {
-		console.log ('Error on find all!');
-	  } else {
-		res.send(result);
-	  };
-	});
+      if (err) {
+        console.log ('Error on find all!');
+      } else {
+        res.send(result);
+      };
+    });
 };
 
 // Retrieve only one message by id from the database
@@ -42,12 +42,12 @@ exports.findById = function(req, res) {
     var id = req.params.id;
     console.log('Retrieving message: ' + id);
     Message.find({ _id: id }).exec(function(err, result) {
-	  if (err) {
-		console.log ('Error on find by id!');
-	  } else {
-		res.send(result);
-	  };
-	});
+      if (err) {
+        console.log ('Error on find by id!');
+      } else {
+        res.send(result);
+      };
+    });
 };
 
 // Add a new message to the database
@@ -55,14 +55,14 @@ exports.findById = function(req, res) {
 exports.addMessage = function(req, res) {
     var messageJSON = req.body;
     console.log('Adding message: ' + JSON.stringify(messageJSON));
-	var messageToPersist = new Message(messageJSON);
-	messageToPersist.save(function (err) { 
-		if (err) {
-			console.log ('Error on save!');
-		} else {
-			// added
-		}
-	});
+    var messageToPersist = new Message(messageJSON);
+    messageToPersist.save(function (err) {
+        if (err) {
+            console.log ('Error on save!');
+        } else {
+            // added
+        }
+    });
 }
 
 // Delete a message from the database based on its id
@@ -70,29 +70,29 @@ exports.addMessage = function(req, res) {
 exports.deleteMessage = function(req, res) {
     var id = req.params.id;
     console.log('Deleting message: ' + id);
-	Message.remove({ _id: id }, function (err) {
-	  if (err) {
-		return handleError(err);
-	  } else {
-		// removed
-	  }
-	});
+    Message.remove({ _id: id }, function (err) {
+      if (err) {
+        return handleError(err);
+      } else {
+        // removed
+      }
+    });
 }
 
 // Update a message by id
-// Test it using curl: curl -i -X PUT -H "Content-Type: application/json" -d @newmessageUpdate.json http://localhost:3000/messages/558d457d9d63180c2b28011a 
+// Test it using curl: curl -i -X PUT -H "Content-Type: application/json" -d @newmessageUpdate.json http://localhost:3000/messages/558d457d9d63180c2b28011a
 exports.updateMessage = function(req, res) {
     var id = req.params.id;
     var message = req.body;
     console.log('Updating message: ' + id);
     console.log(JSON.stringify(message));
 
-	Message.findByIdAndUpdate(id, message, function (err, place) {
-		if (err) {
-			console.log('Error updating message: ' + err);
-		} else {
-			console.log('' + place + ' document(s) updated');
+    Message.findByIdAndUpdate(id, message, function (err, place) {
+        if (err) {
+            console.log('Error updating message: ' + err);
+        } else {
+            console.log('' + place + ' document(s) updated');
             res.send(place);
-		}
-	});
+        }
+    });
 }
